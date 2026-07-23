f73b47c966ffebca78678c22a72e170cb4793dc2
ksusbel <128513556+ksusbel@users.noreply.github.com>
2026-07-23 23:08:35 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,27 @@
-var slaiderFirst = function() {
-    $('#karusel_1').hide();
-    $('#karusel_2').fadeIn();
-    $('#karusel_3').hide();
+$(document).ready(function() {
+    $('#slider').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    margin: 0,
+    autoplayTimeout: 6000,
+    smartSpeed: 1000,
+    margin: 0,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut'
+    
+});
+});
+
+var slaiderFirst = function() {   
+    $('#karusel_2').show();   
 }
-var slaiderSecond = function() {
-    $('#karusel_1').hide();
-    $('#karusel_2').hide();
-    $('#karusel_3').fadeIn();
+var slaiderSecond = function() { 
+    $('#karusel_3').show();
 }
 var slaiderThird = function() {
-    $('#karusel_1').fadeIn();
-    $('#karusel_2').hide();
-    $('#karusel_3').hide();
+    $('#karusel_1').show();   
 }
+
