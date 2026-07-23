4f40d6aa5c09c23a64a86347190cffab71c1b7e2
ksusbel <128513556+ksusbel@users.noreply.github.com>
2026-07-23 17:02:01 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+var slaiderFirst = function() {
+    $('#karusel_1').hide();
+    $('#karusel_2').fadeIn();
+    $('#karusel_3').hide();
+}
+var slaiderSecond = function() {
+    $('#karusel_1').hide();
+    $('#karusel_2').hide();
+    $('#karusel_3').fadeIn();
+}
+var slaiderThird = function() {
+    $('#karusel_1').fadeIn();
+    $('#karusel_2').hide();
+    $('#karusel_3').hide();
+}
