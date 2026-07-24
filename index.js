9a1917a5ab62d969eb6f653a8baf2d97933285fb
ksusbel <128513556+ksusbel@users.noreply.github.com>
2026-07-24 11:08:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('#slider').owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    margin: 0,
+    autoplayTimeout: 6000,
+    smartSpeed: 1000,
+    margin: 0,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut'    
+});
+});
+
+
+
